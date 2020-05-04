2acc82bdbc17b9cc481a431e305f2576cee10105
Teddy Wilson <teddy@toeboat.io>
2020-05-03 22:01:34 -0400

    Clean up for deploy (but somehow display list is not broken)

--- gatsby-config.js
+++ gatsby-config.js
@@ -95,13 +95,7 @@ module.exports = {
       }
     },
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-material-ui`,
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`
-      }
-    }
+    `gatsby-plugin-material-ui`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
