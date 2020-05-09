5f3c4c748348a51d79cf3552c1c6be6bd2870c86
Teddy Wilson <teddy@toeboat.io>
2020-05-09 11:09:06 -0400

    Add tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -78,7 +78,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `G-3B022DYW9E`
       }
     },
     {
