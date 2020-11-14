2e2b5fee327242632ce5d28e24b4885a3bf88593
bingbongle <contact@defund12.org>
2020-11-13 21:35:35 -0500

    Remove comment

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,28 +51,6 @@ module.exports = {
         name: `global`,
       },
     },
-    // {
-    //   resolve: `gatsby-transformer-remark`,
-    //   options: {
-    //     plugins: [
-    //       {
-    //         resolve: `gatsby-remark-images`,
-    //         options: {
-    //           maxWidth: 500
-    //         }
-    //       },
-    //       {
-    //         resolve: `gatsby-remark-responsive-iframe`,
-    //         options: {
-    //           wrapperStyle: `margin-bottom: 1.0725rem`
-    //         }
-    //       },
-    //       `gatsby-remark-prismjs`,
-    //       `gatsby-remark-copy-linked-files`,
-    //       `gatsby-remark-smartypants`
-    //     ]
-    //   }
-    // },
     `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
