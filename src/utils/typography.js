5d7e1ea3f9ca017ba4a6de6e6f80d3289b4e2ce5
bingbongle <contact@defund12.org>
2020-11-14 09:16:35 -0500

    Disable typography hot reload

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -4,8 +4,8 @@ import Wordpress2016 from "typography-theme-wordpress-2016"
 Wordpress2016.overrideThemeStyles = () => {
   return {
     "a.gatsby-resp-image-link": {
-      boxShadow: `none`
-    }
+      boxShadow: `none`,
+    },
   }
 }
 
@@ -14,9 +14,9 @@ delete Wordpress2016.googleFonts
 const typography = new Typography(Wordpress2016)
 
 // Hot reload typography in development.
-if (process.env.NODE_ENV !== `production`) {
-  typography.injectStyles()
-}
+// if (process.env.NODE_ENV !== `production`) {
+//  typography.injectStyles()
+// }
 
 export default typography
 export const rhythm = typography.rhythm
